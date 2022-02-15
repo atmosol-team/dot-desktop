3cf4980c55e055753be69481c487e5ae0420aaba
Thom Stricklin <tstricklin@atmosol.com>
2022-02-15 00:55:10 -0700

    Fix BC Stencil setup

--- setup/bc-stencil.sh
+++ setup/bc-stencil.sh
@@ -5,8 +5,12 @@ if [ ! "$(command -v nvm)" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 fi
 
-# Configure NVM for Stencil
+# Initialize NVM
 export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm nvm install 12
+
+# Configure NVM for Stencil
 nvm install 12
 nvm use 12
 nvm alias default 12
