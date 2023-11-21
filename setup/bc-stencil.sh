fc7c8091262e3043917be94b7766ff3c4742b5f1
Thom Stricklin <tstricklin@atmosol.com>
2023-11-21 11:17:22 -0700

    Update BC stencil node version support

--- setup/bc-stencil.sh
+++ setup/bc-stencil.sh
@@ -11,11 +11,13 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm
 
 # Configure NVM for Stencil
-nvm install 12
-nvm use 12
-nvm alias default 12
+nvm install 18
+nvm use 18
+nvm alias default 18
 
 # Install Stencil CLI
 if [ ! "$(command -v stencil)" ]; then
     npm install -g @bigcommerce/stencil-cli
+else
+    npm update -g @bigcommerce/stencil-cli
 fi
